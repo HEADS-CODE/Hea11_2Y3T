c6fc8693d0a16e0c06aa7bfef00d2ec69958b064
HEADS_KAPECODER <hannah042706@gmail.com>
2026-04-21 07:03:50 +0800

    4-21-2026, 7AM: First time to Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First Ever PHP Activity in Hea11</title>
+</head>
+<body>
+    <div class="header">
+        <h1> This is my first ever PHP Actvitiy in this laptop </h1>
+        <p> PHP is fun </p>
+    </div>
+    
+</body>
+</html>
