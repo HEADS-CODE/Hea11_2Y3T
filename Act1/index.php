4d87e608bc6c6d3024cf59a0840c02a7b120be94
HEADS_KAPECODER <hannah042706@gmail.com>
2026-04-27 11:08:57 +0800

    4-27-2026, 10AM: Sampling First Activity

--- Act1/index.php
+++ Act1/index.php
@@ -3,9 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>My Resume</title>
 </head>
 <body>
+    <?php 
+        $name = "Hannah Elisha A. Delos Santos";
+        $phoneNum = "+63-427-672324";
+        $email = "hadelossantos@fit.edu.ph";
+        $location = "Caloocan City, Philippines";
+    ?>
+
+    <div class="container">
+        <div class="header">
+            <h1><?php echo $name; ?></h1>
+            <hr>
+            <p>Email: <?= $email;?> | Phone: <?= $phoneNum; ?></p>
+        </div>
+    </div>
+
+    <div class="main">
+        <div class="top-section">
+            <img src="profile.png" alt="" class="">
+            <p class="p1"></p>
+        </div>
+    </div>
+    
     
 </body>
 </html>
