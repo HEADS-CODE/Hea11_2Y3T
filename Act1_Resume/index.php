656ddd99f3d955ea8e407b1c56deee7c2598f3a0
HEADS_KAPECODER <hannah042706@gmail.com>
2026-05-01 18:03:40 +0800

    5-1-2026, 6PM: Complete HTML Layout

---
+++ Act1_Resume/index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="styles.css"">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="full-name">
+                <span class="first-name">Hannah Elisha</span>
+                <span class="last-name">Delos Santos</span>
+            </div>
+            <div class="contact-info">
+                <span class="email">Email:</span>
+                <span class="email-val">hadelossantos@fit.edu.ph</span>
+                <span class="seperator"></span>
+                <span class="phone">Phone: </span>
+                <span class="phone-val">+63 9324564324</span>
+            </div>
+
+            <div class="about">
+                <span class="position">Front-End Developer</span>
+                <span class="desc">
+                    I am a front-end developer with more than 3 years of experience writing html, css, and js. I'm motivated, result-focused and seeking a successful team-oriented company with opportunity to grow.
+                </span>
+            </div>
+        </div>
+        <div class="details">
+            <div class="section">
+                <div class="section_title">Experience</div>
+                <div class="section_list">
+                    <div class="section_list-item">
+                        <div class="left">
+                            <div class="name">KlowdBox</div>
+                            <div class="addr">San Fr, CA</div>
+                            <div class="duration">Jan 2011 - Feb 2015</div>
+                        </div>
+                        <div class="right">
+                            <div class="name">Fr developer</div>
+                            <div class="desc">did this and that</div>
+                        </div>
+                    </div>
+                    <div class="section_list-item">
+                        <div class="left">
+                            <div class="name">Akount</div>
+                            <div class="addr">San Monica, CA</div>
+                            <div class="duration">Jan 2011 - Feb 2015</div>
+                        </div>
+                        <div class="right">
+                            <div class="name">Fr developer</div>
+                            <div class="desc">did This and that</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="section">
+                <div class="section_title">Education</div>
+                <div class="section_list">
+                    <div class="section_list-item">
+                        <div class="left">
+                            <div class="name">Sample Institute of technology</div>
+                            <div class="addr">San Fr, CA</div>
+                            <div class="duration">Jan 2011 - Feb 2015</div>
+                        </div>
+                        <div class="right">
+                            <div class="name">Fr developer</div>
+                            <div class="desc">did this and that</div>
+                        </div>
+                    </div>
+                    <div class="section_list-item">
+                        <div class="left">
+                            <div class="name">Akount</div>
+                            <div class="addr">San Monica, CA</div>
+                            <div class="duration">Jan 2011 - Feb 2015</div>
+                        </div>
+                        <div class="right">
+                            <div class="name">Fr developer</div>
+                            <div class="desc">did this and that</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="section">
+                <div class="section_title">Projects</div>
+                <div class="list">
+                    <div class="section_list-item">
+                        <div class="name">DSP</div>
+                        <div class="text">I am a front-end developer with more than 3 years of experience writing html, css, and js. I'm motivated, result-focused and seeking a successful team-oriented company with opportunity to grow.</div>
+                    </div>
+                    <div class="section_list-item">
+                        <div class="name">DSP</div>
+                        <div class="text">I am a front-end developer with more than 3 years of experience writing html, css, and js. I'm motivated, result-focused and seeking a successful team-oriented company with opportunity to grow.<a href="/login">link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section">
+                <div class="section_title">Skills</div>
+                <div class="skills">
+                    <div class="skills_item">
+                        <div class="left">
+                            <div class="name">JavaScript</div>
+                        </div>
+                        <div class="right">
+                            <input id="ck1" type="checkbox" checked/>
+                            <label for="ck1"><label>
+                                <input id="ck2" type="checkbox" checked/>
+                            <label for ="ck2"><label>
+                                <input id="ck3" type="checkbox" checked/>
+                            <label for ="ck3"><label>
+                                <input id="ck4" type="checkbox" checked/>
+                            <label for ="ck4"><label>
+                                <input id="ck5" type="checkbox" checked/>
+                            <label for ="ck5"><label>
+
+                        </div>
+                    </div>
+
+                    <div class="skills_item">
+                        <div class="left"><div class="name">
+                            CSS
+                        </div></div>
+                        <div class="right">
+                            <input id="ck1" type="checkbox" checked/>
+                            <label for="ck1"><label>
+                                <input id="ck2" type="checkbox" checked/>
+                            <label for ="ck2"><label>
+                                <input id="ck3" type="checkbox" checked/>
+                            <label for ="ck3"><label>
+                                <input id="ck4" type="checkbox" checked/>
+                            <label for ="ck4"><label>
+                                <input id="ck5" type="checkbox" checked/>
+                            <label for ="ck5"><label>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="section">
+                    <div class="section_title">
+                        Interests
+                    </div>
+                    <div class="section_list">
+                        <div class="section_list-item">
+                            Football, programming.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    
+</body>
+</html>
