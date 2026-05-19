6bf2a57d1a8206d858986d75158171aaeca61ae6
HEADS_KAPECODER <hea.delossantos.11Prog@gmail.com>
2026-05-19 09:47:32 +0800

    5-19-2026, 9:47AM: Act 1. Complete with good logic (php utilization), style/design, and comment code. Finished Act 1.

--- PSA3_Technical-PHP-Arrays-and-User-Defined-Functions/index.php
+++ PSA3_Technical-PHP-Arrays-and-User-Defined-Functions/index.php
@@ -6,7 +6,7 @@
     <title>My Fruits</title>
     <style>
         body {
-            font-family: Arial, Helvetica, sans-serif;
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
         }
 
         table {
@@ -17,18 +17,19 @@
 
         th, td {
             border: 1px solid black;
-            padding: 10px;
+            padding: 0;
             text-align: center;
             font-size: 20px;
         }
 
         th {
-            background-color: #f2f2f2;
+            background-color: #ffa1fc;
+            padding: 15px;
         }
 
         img {
-            width: 100%;
-            height: auto;
+            width: 120px;
+            height: 120px;
             object-fit: cover;
             display: block;
         }
@@ -45,13 +46,13 @@
         //Fruit Data Records
         $fruits = array(
             array("banana.png", "Banana", "Color Yellow", "Bananas are a healthful to a balanced diet,as they provide a range of vital nutrients and are a good source of fiber."),
-            array("apple.png", "Apple", "Color Red", "Apples are rich in fiber and vitamin C, making them a healthy snack that supports heart health."),
-            array("orange.png", "Orange", "Color Orange", "Oranges are a great source of vitamin C and help boost the immune system."),
-            array("grape.png", "Grapes", "Color Purple/Green", "Grapes contain antioxidants that support heart health and may reduce inflammation."),
+            array("apple.jpg", "Apple", "Color Red", "Apples are rich in fiber and vitamin C, making them a healthy snack that supports heart health."),
+            array("orange.jpg", "Orange", "Color Orange", "Oranges are a great source of vitamin C and help boost the immune system."),
+            array("grapes.jpg", "Grapes", "Color Purple/Green", "Grapes contain antioxidants that support heart health and may reduce inflammation."),
             array("mango.png", "Mango", "Color Yellow/Orange", "Mangoes are rich in vitamins A and C and are known for their sweet taste."),
-            array("pineapple.png", "Pineapple", "Color Yellow", "Pineapple contains bromelain, an enzyme that aids digestion and reduces inflammation."),
-            array("watermelon.png", "Watermelon", "Color Red/Green", "Watermelon is hydrating and packed with vitamins A and C, perfect for hot weather."),
-            array("strawberry.png", "Strawberry", "Color Red", "Strawberries are rich in antioxidants and may improve heart health."),
+            array("pineapple.jpg", "Pineapple", "Color Yellow", "Pineapple contains bromelain, an enzyme that aids digestion and reduces inflammation."),
+            array("watermelon.jpg", "Watermelon", "Color Red/Green", "Watermelon is hydrating and packed with vitamins A and C, perfect for hot weather."),
+            array("strawberry.jpg", "Strawberry", "Color Red", "Strawberries are rich in antioxidants and may improve heart health."),
             array("papaya.png", "Papaya", "Color Orange", "Papayas contain digestive enzymes and are rich in vitamin C and beta-carotene."),
             array("kiwi.png", "Kiwi", "Color Green", "Kiwis are nutrient-dense and high in vitamin C, potassium, and fiber.")
         );
@@ -61,11 +62,14 @@
     ?>
 
     <div class="container">
+        <!-- Display Table -->
         <table>
+             <!-- Title -->
             <tr>
-                <th colspan="4">My Fruits</th>
+                <th colspan="4" style="background-color: purple; color: white;">My Fruits</th>
             </tr>
-            <tr>
+            <!-- Columns -->
+            <tr style="color: purple;">
                 <th>Image</th>
                 <th>Name</th>
                 <th>Description</th>
@@ -78,9 +82,9 @@
             foreach ($fruits as $fruit){
                 echo "<tr>";
                 echo "<td><img src='images/" . $fruit[0] . "'></td>";
-                echo "<td>" . $fruit[1] . "</td>";
-                echo "<td>" . $fruit[2] . "</td>";
-                echo "<td>" . $fruit[3] . "</td>";
+                echo '<td style="padding: 20px;">' . $fruit[1] . "</td>";
+                echo '<td style="padding: 20px;">' . $fruit[2] . "</td>";
+                echo '<td style="padding: 20px;">' . $fruit[3] . '</td>';
                 echo "</tr>";
             }
             
