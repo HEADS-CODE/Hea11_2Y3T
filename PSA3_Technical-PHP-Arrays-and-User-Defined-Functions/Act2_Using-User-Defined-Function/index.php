5b64d4893b74c46d6cdcaac3f3dbdf5c6a7ac5dc
HEADS_KAPECODER <hea.delossantos.11Prog@gmail.com>
2026-05-19 10:12:02 +0800

    5-19-2026, 10:12AM: Act 2 Complete with good PHP logic use and simple design/styling. Includes comment codes. Finished Activity.

--- PSA3_Technical-PHP-Arrays-and-User-Defined-Functions/Act2_Using-User-Defined-Function/index.php
+++ PSA3_Technical-PHP-Arrays-and-User-Defined-Functions/Act2_Using-User-Defined-Function/index.php
@@ -5,6 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Volume of Shapes</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin: 20px auto;
+            font-family: Arial;
+        }
+
+        td, th {
+            border: 1px solid black;
+            padding: 10px;
+            text-align: center;
+        }
+
+        th {
+            background-color: #ffa1fc;
+        }
+    </style>
 </head>
 <body>
 
@@ -13,7 +31,7 @@
     function volume_table($values, $formula, $answer)
     {
         echo "<table>";
-        echo "<tr><th colspan='3'>Volume of Shapes</th></tr>";
+        echo '<tr><th colspan="3" style="background-color: purple; color: white;">Volume of Shapes</th></tr>';
         echo "<tr><th>Values</th><th>Formula</th><th>Answer</th></tr>";
         echo "<tr>";
         echo "<td>$values</td>";
@@ -28,11 +46,11 @@
     $s = 5;
     volume_table("s = $s", "V = s³", $s * $s * $s);
 
-    //Rectangular Prism: V = l × w × h
+    //Rectangular Prism: V = l x w x h
     $l = 4;
     $w = 3;
     $h = 2;
-    volume_table("l=$l, w=$w, h=$h", "V = l×w×h", $l * $w * $h);
+    volume_table("l=$l, w=$w, h=$h", "V = lxwxh", $l * $w * $h);
 
     //Cylinder: V = πr^2h
     $r = 3;
