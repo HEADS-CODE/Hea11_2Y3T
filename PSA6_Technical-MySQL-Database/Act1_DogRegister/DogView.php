4f4a43d1b6a0d0df99a4735b412b301aee852290
HEADS_KAPECODER <hea.delossantos.11Prog@gmail.com>
2026-06-22 12:36:19 +0800

    6-22-2026, 12:36PM: DogView.php. Finalize both HTML body with PHP code and CSS. Ready to pass all files :D!

--- PSA6_Technical-MySQL-Database/Act1_DogRegister/DogView.php
+++ PSA6_Technical-MySQL-Database/Act1_DogRegister/DogView.php
@@ -1,4 +1,5 @@
 <?php include 'db_DogInfo.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +7,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dog Record Directory</title>
     <link href="https://fonts.googleapis.com/css2?family=Readex+Pro&display=swap" rel="stylesheet">
+    <style>
+        body {
+            background-color: #2e1a47;
+            font-family: 'Readex Pro', sans-serif;
+            padding: 50px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        h2 {
+            color: white;
+            margin-bottom: 20px;
+        }
+        .main-box {
+            border-radius: 15px;
+            padding: 0px;
+            color: black;
+            width: 500px;
+        }
+        .result-box {
+            border: 2px solid #7a4bff;
+            border-radius: 10px;
+            padding: 20px;
+            margin-top: 20px;
+            background-color: #f4efff;
+            line-height: 1.6;
+        }
+        .dog-count {
+            font-weight: bold;
+            color: #2e1a47;
+            border-bottom: 1px solid #7a4bff;
+            margin-bottom: 10px;
+            display: block;
+        }
+        .footer {
+            font-size: 12px;
+            margin-top: 20px;
+            color: #ccc;
+        }
+    </style>
 </head>
+
 <body>
     <h2>Dog Record Directory</h2>
     <div class="main-box">
@@ -38,7 +80,7 @@
         ?>
     </div>
     <div class="footer">
-        @Dog Directory by <b>Hannah Elisha A. Delos Santos</b>
+        @ Dog Directory by <b>Hannah Elisha A. Delos Santos</b>
     </div>
 </body>
 </html>
